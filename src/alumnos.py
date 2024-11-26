87419ce856be0d5d63ba12aedacd280746671319
Cesar Balam <cesarbalam.2004@gmail.com>
2024-11-25 22:00:51 -0600

    Validacion para no poder agregar la misma materia mas de 1 vez en preregistro

--- src/alumnos.py
+++ src/alumnos.py
@@ -243,7 +243,10 @@ class AlumnosFrame(tk.Frame):
             messagebox.showerror("Error", "No se ha seleccionado ningun grupo")
             return
         
+        materia_a_agregar = re.search(r'(.+) \(ID:\d+\)', self.combo_materias_disponibles.get()).group(1)
+        
         ids_grupos_seleccionados = [re.search(r'\(ID:(\d+)\)', grupo).group(1) for grupo in self.lista_combo_seleccionadas]
+        nombres_materias_seleccionadas = [re.search(r'(.+) \(ID:\d+\)', grupo).group(1) for grupo in self.lista_combo_seleccionadas]
         
         id_grupo_a_agregar = re.search(r'\(ID:(\d+)\)', self.combo_materias_disponibles.get()).group(1)
         
@@ -273,8 +276,11 @@ class AlumnosFrame(tk.Frame):
             if result:
                 messagebox.showerror("Error", f"""No se puede agregar este grupo\nEl horario del grupo {result[0][0]} de {result[0][3]} a {result[0][4]} el {result[0][2]}\nEntra en conflicto con el grupo {result[0][1]} de {result[0][5]} a {result[0][6]} el {result[0][7]}\n""")
                 return
+            
+        if materia_a_agregar in nombres_materias_seleccionadas:
+            messagebox.showerror("Error", "No se puede agregar la misma materia dos veces")
+            return
         
-        self.combo_materias_disponibles.get()
         self.lista_combo_seleccionadas.append(self.combo_materias_disponibles.get())
         self.combo_materias_seleccionadas.config(values=self.lista_combo_seleccionadas)
         self.lista_combo_disponibles.remove(self.combo_materias_disponibles.get())
